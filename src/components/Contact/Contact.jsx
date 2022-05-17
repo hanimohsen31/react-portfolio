f04407a7f2e32160fc491e2c07844f67d696f962
hanimohsen31 <hanimohsen3810@yahoo.com>
2022-05-17 14:19:09 +0200

    Finish V.1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,35 @@
-import './Contact.scss'
+import "./Contact.scss";
 
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div className='Contact' id='Contact'>
-        Contact
+    <div className="Contact" id="Contact">
+      <div className="container">
+        <div className="left">
+          <img src="assets/imgs/shake.svg" alt="" />
+        </div>
+
+        <div className="right">
+          <h2>Contact.</h2>
+          <form action="">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" placeholder="Email" />
+            </div>
+
+            <div>
+              <label htmlFor="msg">Message</label>
+              <textarea name="msg" id="msg" cols="30" rows="10"></textarea>
+            </div>
+
+            <button type="submit" className="button" onSubmit={handleSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,7 +1,51 @@
-import './Testimonials.scss'
+import "./Testimonials.scss";
 
 export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Tom Durden",
+      title: "Senior Developer",
+      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/imgs/twitter.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Alex Kalinski",
+      title: "Co-Founder of DELKA",
+      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/imgs/youtube.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit Placeat magnam.",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/imgs/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
   return (
-    <div className='Testimonials' id='Testimonials'>Testimonials</div>
-  )
+    <div className="Testimonials" id="Testimonials">
+      {data.map((p) => (
+        <div className={p.featured ? "featured container" : "container"}>
+          <div className="top">
+            <img src="assets/imgs/right-arrow.png" alt="" />
+            <img className="user" src={p.img} alt="" />
+            <img src={p.icon} alt="" />
+          </div>
+          <div className="middle">
+            <p>{p.desc}</p>
+          </div>
+          <div className="lower">
+            <h3>{p.name}</h3>
+            <h4>{p.title}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
